c35480beb3c33da0890d5b5794fde7298a06545c
fabrica.s2 <fabrica.s2@basis.com.br>
2021-12-21 12:40:45 -0300

    Corrigir erros do sonar - BASIS-241479

--- backend/src/main/java/br/com/basis/abaco/domain/DivergenceComment.java
+++ backend/src/main/java/br/com/basis/abaco/domain/DivergenceComment.java
@@ -4,7 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.SequenceGenerator;
 import java.io.Serializable;
 
 @MappedSuperclass
