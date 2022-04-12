c64253a5bb5530ad1823823949d7c6c9a70fd879
fabrica.s2 <fabrica.s2@basis.com.br>
2022-04-12 09:08:23 -0300

    Realizar ajustes na BASIS-263935

--- backend/src/main/java/br/com/basis/abaco/service/HistoricoService.java
+++ backend/src/main/java/br/com/basis/abaco/service/HistoricoService.java
@@ -76,6 +76,7 @@ public class HistoricoService {
         historicoRepository.save(historico);
     }
 
+    
     public ByteArrayOutputStream gerarRelatorio(AnaliseDTO analise, String tipoRelatorio) throws RelatorioException {
         ByteArrayOutputStream byteArrayOutputStream;
         try {
